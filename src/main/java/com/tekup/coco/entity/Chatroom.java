17a0eabbce70b22ac8282ec9d18e04b63f33ca3c
Hatem Boudabra <hatemboudabra41@gmail.com>
2024-05-14 11:30:16 +0200

    fixed chatroom

--- src/main/java/com/tekup/coco/entity/Chatroom.java
+++ src/main/java/com/tekup/coco/entity/Chatroom.java
@@ -1,5 +1,6 @@
 package com.tekup.coco.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
--- src/main/java/com/tekup/coco/entity/Chatrromassistance.java
+++ src/main/java/com/tekup/coco/entity/Chatrromassistance.java
@@ -1,5 +1,6 @@
 package com.tekup.coco.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
